03aa295466b13ebe629632aebfb0ea50233d422a
Luceatscode <lucsw92@hotmail.com>
2023-08-24 13:02:25 -0600

    changed contact page and about me bio

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -11,12 +11,9 @@ export default function About() {
       <p class="bio">
         Thank you for checking out my portfolio! I live in Salt Lake City, Utah but am originally from Los Angeles, California!
         I have a passion for learning and am excited to be learning web development. I am currently enrolled in a full-stack coding bootcamp through the University of Utah.
-        As you will see in my resume, I served in the United States Marine Corps for 4 years and was a Supply Administration and Operations Non-Commissioned Officer. I went to bootcamp 
-        in San Diego, California MCRD and was stationed in Twentynine-Palms, California and I deployed to Kuwait for 8 months. I moved to Utah in 2018 and received my Associates of Science
-        from Salt Lake Community college in 2019. I also was a personal trainer at 24 Hour Fitness before Covid-19 shut our gyms down. I bartended at the prestigious Snowbird Ski Resort in their
-        fine dining establishment, The Aerie. I then started working at a tech start-up company called Route in 2022 doing customer service related to claims. My old Lieutenant from the Marine Corps called me
-        up and offered me an Accounting job with Inspectre Solutions which s a full service investigative and medical case management agency specializing in international workers compensation, Defense Base Act, 
-        war hazard claims and emergency extractions. My time at Route inspired me to take the leap and enroll in a coding bootcamp!
+        You will find my work history and education in my resume but here are some things about me that you won't find there: I love all outdoor activites despite the weather!
+        I have a purple belt in Jiu-Jitsu and love going to the gym for weight-lifting. I love cooking and trying new foods. I am a fan of all Los Angeles sports teams and the Utah Jazz.
+        I am a passionate about music and love going to concerts. I have been Dj since 2019 and love playing music for my friends at parties or private events. I also Dj weddings on the side!
         I am looking forward to learning more about web development and am excited to see where this journey takes me!
       </p>
     </div>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,21 +2,31 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div class= "body">
+    <div class="body">
       <h1>Contact Page</h1>
       <form>
-      <h2>Let's get in touch!</h2>
-      <input type="text" id="name" placeholder='Your Name' required></input>
-      <input type="email" id="email" placeholder='Your Email' required></input>
-      <textarea id="message" rows="7" placeholder='Your Message' required></textarea>
-      <button type="submit">Submit</button>
+        <div class="form-group">
+          <label for="exampleInputName1">Name</label>
+          <input type="name" class="form-control" id="exampleInputName1" aria-describedby="emailHelp" placeholder="Enter Full Name"></input>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter Email"></input>
+          <small id="emailHelp" class="form-text text-muted">I'll never share your email with anyone else!</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputMessage1">Send me a message!</label>
+          <input type="message" class="form-control" id="exampleInputMessage1" placeholder="Message"></input>
+          <small id="emailHelp" class="form-text text-muted">Leave me a message at the beep! Beep!</small>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
       </form>
 
       <ul>
-        <li class= "text">Phone: 661-754-1744</li>
-        <li class= "text">Email: lucsw92@hotmail.com</li>
-        <li class= "text">GitHub: <a href="https://github.com/Luceatscode">Luceatscode</a></li>
-        <li class= "text">LinkedIn: <a href="https://www.linkedin.com/in/thomas-luc-russell-4b1421ab/">Luc Russell</a></li>
+        <li class="ContactText">Phone: 661-754-1744</li>
+        <li class="ContactText">Email: lucsw92@hotmail.com</li>
+        <li class="ContactText">GitHub: <a href="https://github.com/Luceatscode">Luceatscode</a></li>
+        <li class="ContactText">LinkedIn: <a href="https://www.linkedin.com/in/thomas-luc-russell-4b1421ab/">Luc Russell</a></li>
       </ul>
     </div>
   );
