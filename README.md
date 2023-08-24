09c50efcf1e387b4fb3cf4a6dde1366a110cbc11
Luceatscode <lucsw92@hotmail.com>
2023-08-24 14:49:09 -0600

    created readme

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # React Portfolio
 
-This will be my react portfolio readme
+## Description
+
+This is my final project for my coding bootcamp! I created a portfolio using react with an About me, Portfolio, Contact, and Resume section.
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Feedback](#feedback)
+  * [Questions](#questions)
+  * [Credits](#credits)
+  * [Deployed Application](#deployedapplication)
+
+
+  ## Installation
+
+  To install, clone the repository. Next, run "npm i" then "npm run start" to open the app in the browser.
+
+  ## Feedback
+
+  Feedback or suggestions are always welcome so please feel free to reach out if you have anything that might help this work better!
+
+  ## Questions
+
+  * [Github](https://github.com/Luceatscode)
+  * Please email me with any questions at lucsw92@hotmail.com
+
+  ## Credits
+
+  Stack Overflow, W3 Schools, and YouTube were online resources I used to get this application working. Additional help was received from my developer friend @joeybuenosdias on GitHub and tutors at the University of Utah Coding Bootcamp. I also used getbootstrap.com for code and coolors.co for my color scheme. Thanks for looking!
+
+  ## Deployed Application
+
+  Here is the link to my deployed application! [Link]()
+
+![Here is my screenshot!](profilePic.jpeg)
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,31 +3,31 @@ import React from 'react';
 export default function Contact() {
   return (
     <div class="body">
-      <h1>Contact Page</h1>
-      <form>
-        <div class="form-group">
-          <label for="exampleInputName1">Name</label>
-          <input type="name" class="form-control" id="exampleInputName1" aria-describedby="emailHelp" placeholder="Enter Full Name"></input>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter Email"></input>
-          <small id="emailHelp" class="form-text text-muted">I'll never share your email with anyone else!</small>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputMessage1">Send me a message!</label>
-          <input type="message" class="form-control" id="exampleInputMessage1" placeholder="Message"></input>
-          <small id="emailHelp" class="form-text text-muted">Leave me a message at the beep! Beep!</small>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
+      <h1>Feel Free to Contact Me!</h1>
+      <form id="myForm">
+  <div class="form-group">
+    <label for="exampleInputName1">Name</label>
+    <input type="name" class="form-control" id="exampleInputName1" aria-describedby="emailHelp" placeholder="Enter Full Name"></input>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter Email"></input>
+    <small id="emailHelp" class="form-text text-muted">I'll never share your email with anyone else!</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputMessage1">Send me a message!</label>
+    <input type="message" class="form-control" id="exampleInputMessage1" placeholder="Message"></input>
+    <small id="emailHelp" class="form-text text-muted">Leave me a message at the beep! Beep!</small>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 
-      <ul>
-        <li class="ContactText">Phone: 661-754-1744</li>
-        <li class="ContactText">Email: lucsw92@hotmail.com</li>
-        <li class="ContactText">GitHub: <a href="https://github.com/Luceatscode">Luceatscode</a></li>
-        <li class="ContactText">LinkedIn: <a href="https://www.linkedin.com/in/thomas-luc-russell-4b1421ab/">Luc Russell</a></li>
-      </ul>
-    </div>
+    <ul>
+      <li class="ContactText">Phone: 661-754-1744</li>
+      <li class="ContactText">Email: lucsw92@hotmail.com</li>
+      <li class="ContactText">GitHub: <a href="https://github.com/Luceatscode">Luceatscode</a></li>
+      <li class="ContactText">LinkedIn: <a href="https://www.linkedin.com/in/thomas-luc-russell-4b1421ab/">Luc Russell</a></li>
+    </ul>
+    </div >
   );
 }
